17663529fb66bb18c8f918a51731dd17f1dd34c7
coderkoosh <koosha.tablet+github@gmail.com>
2026-06-05 11:55:57 +0200

    FlaskUpdate

---
+++ WordleCheatFlask.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from datetime import datetime
+import json
+from urllib import request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    dt = datetime.now()
+    link = "https://www.nytimes.com/svc/wordle/v2/" + dt.strftime("%Y-%m-%d") + ".json"
+    urlopen = request.urlopen(link)
+    data = urlopen.read()
+    js = json.loads(data)
+    #print(f"Solution: \n{js['solution']}\nEditor: \n{js['editor']}")    
+    return f"<p>Solution: {js['solution']} Editor: {js['editor']}<p/>"
